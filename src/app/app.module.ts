842d6ce41939183a1359fdb851b0a92ef985267c
Thiago Abad <abadthiago@gmail.com>
2020-09-13 14:49:58 +0100

    feat: add payments HTTP backend support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
@@ -15,7 +16,8 @@ import { StorageService } from './storage.service';
     BrowserModule,
     WidgetsModule,
     AppRoutingModule,
-    ErrorsModule
+    ErrorsModule,
+    HttpClientModule
   ],
   providers: [StorageService],
   bootstrap: [AppComponent]
--- src/app/widgets/payments/payments.service.ts
+++ src/app/widgets/payments/payments.service.ts
@@ -1,24 +1,23 @@
 import { Injectable } from '@angular/core';
-//import { HttpClient } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { Payment } from './payment';
 import { BehaviorSubject } from 'rxjs';
 
-const API = 'http://localhost:3000';
+const API = 'https://thiagoa.free.beeceptor.com';
 
 @Injectable({ providedIn: 'root' })
 export class PaymentsService {
 
-    // constructor(private http: HttpClient) {}
-    constructor(){}
+    constructor(private http: HttpClient) {}
 
     payments: Payment[] = [];
     private paymentsBS = new BehaviorSubject(this.payments);
     sharedPayments = this.paymentsBS.asObservable();
 
-    // apiPut() {
-    //     return this.http
-    //         .put<Payment[]>(API + '/payments', this.payments);
-    // }
+    apiPut() {
+         return this.http
+             .put<Payment[]>(API + '/payments', this.payments).subscribe();
+    }
 
     nextPayments(payments: Payment[]){
         this.paymentsBS.next(payments);
